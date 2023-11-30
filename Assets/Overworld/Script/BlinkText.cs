1b182b5a5920c9c126a8740556a7790016d9d012
junyoung <ocg9884@naver.com>
2023-12-01 04:23:04 +0900

    *Wold*

--- Assets/Overworld/Script/BlinkText.cs
+++ Assets/Overworld/Script/BlinkText.cs
@@ -20,7 +20,7 @@ public class BlinkText : MonoBehaviour
             flashingText.text = "";
             yield return new WaitForSeconds(.5f);
             flashingText.text = "Press Any Button";
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(1.3f);
         }
     }
 }

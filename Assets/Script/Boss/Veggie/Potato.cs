294073c3436719f4a9caf8b8de28c164506acc0b
ParkSe3395 <tpgur1201@gmail.com>
2023-12-01 09:49:36 +0900

    votanic

--- Assets/Script/Boss/Veggie/Potato.cs
+++ Assets/Script/Boss/Veggie/Potato.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Potato : BossBase
 {
     private Animator anim;
+    private int shootCount = 0;
 
-    protected override void runDead()
-    {
-        this.anim.Play("");
-    }
+    [SerializeField]
+    private GameObject defaultBullet;
+    [SerializeField]
+    private GameObject pinkBullet;
+    [SerializeField]
+    private GameObject shoot_fx;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,38 @@ public class Potato : BossBase
     // Update is called once per frame
     void Update()
     {
+        idleTime += Time.deltaTime;
+        Debug.Log(idleTime);
+        if(NowHP == 0)
+            this.anim.Play(DeadAnim);
         
+        if(idleTime >= 3f)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        if(shootCount < 3)
+        {
+            Instantiate(defaultBullet,shoot_fx.transform.position,new Quaternion());
+            shoot_fx.GetComponent<Animator>().Play(EffectAnims[0]);
+            this.shootCount++;
+
+            if (NowHP / MaxHP > 0.5f)
+                yield return new WaitForSeconds(2.0f);
+            else
+                yield return new WaitForSeconds(1.0f);
+        }
+        else
+        {
+            Instantiate (pinkBullet, shoot_fx.transform.position, new Quaternion());
+            shoot_fx.GetComponent<Animator>().Play(EffectAnims[0]);
+            this.shootCount = 0;
+            idleTime = 0;
+
+            yield break;
+        }
     }
 }

18741920fbf0d6214cf89352ab3533d1bda286ba
yeo <ygd08@naver.com>
2023-12-04 12:16:42 +0900

    merge

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,17 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody;
     float axisH = 0.0f;                //좌우 방향키 입력
-    float axisV = 0.0f;                
+    float axisV = 0.0f;
     public float speed = 4.0f;         //이동속도, 3.0f -> 4.0f로 변경
 
     public float jump = 7.0f;          //점프력
     public float dash = 50.0f;          //대쉬력
     public LayerMask groundLayer;       //착지 가능한 레이어
     bool goJump = false;               //점프키 입력상태
-    bool onGround = false;             //지면과 접촉상태
+    public bool onGround = false;             //지면과 접촉상태
     bool goDash = false;               //대쉬 입력 상태
     public bool downJump = false;             //아래 점프
-    
+
+
     //애니메이터
     Animator animator;
     public string stopAnime = "PlayerIdle"; //가만히 있을 때 애니메이션
@@ -46,11 +47,12 @@ public class PlayerController : MonoBehaviour
     public static int hp = 3;
     bool inDamage = false;
     public static PlayerController instance; //싱글톤 패턴에 쓴다?
-    
+    public bool isParry = false;      //패링 상태
     //근데 싱글톤 패턴이 뭐였냐
     private void Awake()
     {
-        if(PlayerController.instance == null)
+        GetComponent<BoxCollider2D>().enabled = false;
+        if (PlayerController.instance == null)
         {
             PlayerController.instance = this;
         }
@@ -60,7 +62,7 @@ public class PlayerController : MonoBehaviour
     {
         // RigidBody 2D 컴포넌트 정보 가져오기
         rbody = this.GetComponent<Rigidbody2D>();
-   
+
 
         animator = GetComponent<Animator>();
         nowAnime = stopAnime;
@@ -71,7 +73,7 @@ public class PlayerController : MonoBehaviour
 
         //HP 불러오기
         hp = PlayerPrefs.GetInt("PlayerHP");
-        
+
     }
 
     // Update is called once per frame
@@ -82,37 +84,37 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-            
+
 
         //수평 방향 입력 ->GetKeyDown으로 모두 바꾸는 게 나을수도
-        if(isMoving == false)
+        if (isMoving == false)
         {
-           axisH = Input.GetAxisRaw("Horizontal");
-           axisV = Input.GetAxisRaw("Vertical");
+            axisH = Input.GetAxisRaw("Horizontal");
+            axisV = Input.GetAxisRaw("Vertical");
         }
         //키 입력을 통해 이동각도를 구하기
         Vector2 fromPt = transform.position;
         Vector2 toPt = new Vector2(fromPt.x + axisH, fromPt.y + axisV);
         angleZ = GetAngle(fromPt, toPt);
 
-       //Input.GetKeyDown(KeySetting.Keys(KeyAction. ......)
+        //Input.GetKeyDown(KeySetting.Keys(KeyAction. ......)
 
         //캐릭터 방향 조절
-        if (Input.GetKeyDown(KeyCode.RightArrow)&&onGround)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && onGround)
         {
             //오른쪽 이동
             Debug.Log("오른쪽 이동");
             transform.localScale = new Vector2(1, 1);
             //나는 8방향 발사보단 8방향 애니메이션을 만들려고 했는데 왜 8방향 발사가 됨?
-            
+
         }
-        if(Input.GetKeyUp(KeyCode.RightArrow)&&onGround)
+        if (Input.GetKeyUp(KeyCode.RightArrow) && onGround)
         {
             Debug.Log("오른쪽 이동키 뗌");
             nowAnime = stopAnime;
             animator.Play(nowAnime);
         }
-        
+
         if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) && onGround)
         {
             // 우대각 방향
@@ -120,23 +122,23 @@ public class PlayerController : MonoBehaviour
             nowAnime = rundiagonlaupAnime;
             animator.Play(nowAnime);
         }
-        
-        if (Input.GetKeyDown(KeyCode.LeftArrow)&&onGround)
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && onGround)
         {
             //왼쪽 이동
             Debug.Log("왼쪽 이동");
             transform.localScale = new Vector2(-1, 1); //좌우 반전
         }
 
-        if(Input.GetKeyUp(KeyCode.LeftArrow)&&onGround)
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && onGround)
         {
             Debug.Log("왼쪽 이동키 뗌");
             nowAnime = stopAnime;
             animator.Play(nowAnime);
-                
+
         }
-        
-        
+
+
         if (Input.GetKeyUp(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow) && onGround)
         {
             Debug.Log("왼쪽 이동키 떼면서 오른쪽으로");
@@ -144,7 +146,7 @@ public class PlayerController : MonoBehaviour
             animator.Play(nowAnime);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow)&&Input.GetKey(KeyCode.UpArrow) && onGround)
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow) && onGround)
         {
             //좌대각 방향 좌대각으로 바라보고 있을 때 점프가 안되는데 왜지?
             Debug.Log("좌대각");
@@ -152,14 +154,14 @@ public class PlayerController : MonoBehaviour
             animator.Play(nowAnime);
         }
         //(KeySetting.keys[KeyAction.Shot])
-        if (Input.GetKeyDown(KeyCode.Z)&&onGround)
+        if (Input.GetKeyDown(KeyCode.Z) && onGround)
         {
             Debug.Log("서 있으면서 발사");
             nowAnime = shootstraightAnime;
             animator.Play(nowAnime);
-          
+
         }
-        if(Input.GetKeyUp(KeyCode.Z) && onGround)
+        if (Input.GetKeyUp(KeyCode.Z) && onGround)
         {
             Debug.Log("z키에서 손 땜");
             nowAnime = stopAnime;
@@ -167,20 +169,20 @@ public class PlayerController : MonoBehaviour
 
         }
 
-        if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Z) && axisV ==0 &&onGround)
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Z) && axisV == 0 && onGround)
         {
             Debug.Log("오른쪽 이동하면서 발사");
             transform.localScale = new Vector2(1, 1);
             nowAnime = runshootstraightAnime;
             animator.Play(nowAnime);
-            
+
         }
         if (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyUp(KeyCode.Z) && onGround)
         {
             nowAnime = runAnime;
             animator.Play(nowAnime);
         }
-       
+
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Z) && axisV == 0 && onGround)
         {
             Debug.Log("왼쪽 이동하면서 발사");
@@ -195,8 +197,8 @@ public class PlayerController : MonoBehaviour
         }
 
 
-            //캐릭터 점프
-            if (Input.GetButtonDown("Jump"))
+        //캐릭터 점프
+        if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
@@ -206,9 +208,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("위쪽 방향");
             nowAnime = aimupAnime;
             animator.Play(nowAnime);
-         }
+        }
         //윗 방향키에서 손을 뗐을 때 애니메이션 바꾸기
-        if(Input.GetKeyUp(KeyCode.UpArrow) && onGround)
+        if (Input.GetKeyUp(KeyCode.UpArrow) && onGround)
         {
             Debug.Log("윗 방향키 손 땜");
             nowAnime = stopAnime;
@@ -223,14 +225,14 @@ public class PlayerController : MonoBehaviour
             nowAnime = shootupAnime;
             animator.Play(shootupAnime);
             Debug.Log("윗방향키, Z키 누름");
-            
+
         }
-       //이렇게 if문이 수도 없이 늘어나는게 맞나????
-       if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyUp(KeyCode.Z) && onGround)
+        //이렇게 if문이 수도 없이 늘어나는게 맞나????
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyUp(KeyCode.Z) && onGround)
         {
             nowAnime = aimupAnime;
             animator.Play(nowAnime);
-            Debug.Log("윗방향키, Z키 땜");       
+            Debug.Log("윗방향키, Z키 땜");
         }
         if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.Z) && onGround)
         {
@@ -240,7 +242,7 @@ public class PlayerController : MonoBehaviour
             animator.Play(nowAnime);
         }
         //우대각으로 조준하며 움직이던 중 윗 방향키에서 손 때면
-        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyUp(KeyCode.UpArrow) &&onGround)
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyUp(KeyCode.UpArrow) && onGround)
         {
             Debug.Log("윗방향키 손 땜");
             nowAnime = runAnime;
@@ -260,12 +262,12 @@ public class PlayerController : MonoBehaviour
             nowAnime = duckidleAnime;
             animator.Play(nowAnime);
         }
-        else if(Input.GetKeyUp(KeyCode.DownArrow) && onGround)
+        else if (Input.GetKeyUp(KeyCode.DownArrow) && onGround)
         {
             nowAnime = stopAnime;
             animator.Play(nowAnime);
         }
-        if(Input.GetKey(KeyCode.DownArrow)&&Input.GetKeyDown(KeyCode.Z) && onGround)
+        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Z) && onGround)
         {
             nowAnime = duckshootAnime;
             animator.Play(nowAnime);
@@ -276,21 +278,21 @@ public class PlayerController : MonoBehaviour
             animator.Play(nowAnime);
         }
         //대쉬, x키를 눌렀을 때 대쉬 애니메이션을 그냥 블링크로 바꾸자
-        if(Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))
         {
             Dash();
             nowAnime = dustAnime;
             animator.Play(nowAnime);
-            
+
         }
 
-        if(Input.GetKey(KeyCode.DownArrow)&&Input.GetKey(KeyCode.Space)&&onGround)
+        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.Space) && onGround)
         {
             DownJump();
 
         }
 
-       
+
 
         //z키를 눌렀을 때 발사하는 애니메이션, 인풋매니저 설정 안바꿔서 임시로 z
         /*
@@ -307,7 +309,11 @@ public class PlayerController : MonoBehaviour
             nowAnime = stopAnime;
             animator.Play(nowAnime);
         }*/
-
+        if (onGround == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            Parry();
+            Invoke("ParryEnd", 0.25f);
+        }
 
     }
     //private 가 앞에 붙어있어서 없애봤지만 여전히 착지 판정 처리가 이상하다->linecast의 y축 값을 조정하니 판정 처리가 괜찮아졌다.
@@ -317,24 +323,24 @@ public class PlayerController : MonoBehaviour
             return;
         //착지 판정 처리
         onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 1.0f), groundLayer);
-        
+
         //이 if문이 있을 때 플레이어 캐릭터가 움직이긴 하나 캐릭터가 엎어졌다.
         //Rigidbody 2D의 Constraints의 Freeze Rotation의 z축을 체크하자 캐릭터가 엎어지진 않았으나 의도치않게 가속도가 붙는다.
-        if (onGround || axisH !=0)
+        if (onGround || axisH != 0)
         {
             //캐릭터가 지면에 있거나 x축 입력값이 0이 아닐 경우 velocity 변경
             rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
         }
         //이 if문까지 넣었을 때 Jump 함수가 활성화 되나 캐릭터가 점프하진 않았다.
-        if(onGround && goJump)
+        if (onGround && goJump)
         {
             Debug.Log("점프!");
             Vector2 jumpPw = new Vector2(0, jump);            //점프를 위한 벡터
             rbody.AddForce(jumpPw, ForceMode2D.Impulse);      //순간적인 힘을 가한다
             goJump = false;  //점프 플래그 off
-                                                                              
+
         }
-        if(onGround && downJump)
+        if (onGround && downJump)
         {
             Debug.Log("다운 점프!");
             Vector2 jumpPw = new Vector2(0, -jump);
@@ -343,7 +349,7 @@ public class PlayerController : MonoBehaviour
         }
         //이 if문을 넣었을 때 대쉬 함수와 이 if문이 활성화 되긴 하나 대쉬가 되진 않았다 대쉬의 기본값을 크게 늘려주자 대쉬를 했다..
         //대쉬를 연속적으로 못하게 하고 싶긴하지만 나중에 시간 날때 구현하자.
-        if(goDash)
+        if (goDash)
         {
             Debug.Log("대쉬!");
             //rbody.velocity = new Vector2(axisH * dash, rbody.velocity.y);
@@ -353,31 +359,31 @@ public class PlayerController : MonoBehaviour
 
 
         }
-        
-        if(onGround)
+
+        if (onGround)
         {
             //지면과 맞닿아있을 때
-            
+
             if (axisH == 0)
                 nowAnime = "PlayerIdle";
-            
+
             else
                 nowAnime = "PlayerRun";
-            
+
         }
         else
         {
             //공중에 있을 때 점프 애니메이션
-            nowAnime = jumpAnime; 
+            nowAnime = jumpAnime;
         }
-        
-        if(nowAnime !=oldAnime)
+
+        if (nowAnime != oldAnime)
         {
             oldAnime = nowAnime;
             animator.Play(nowAnime); //애니메이션 재생 
         }
 
-        
+
     }
 
     //점프 트리거 on
@@ -391,11 +397,11 @@ public class PlayerController : MonoBehaviour
     {
         goDash = true;
         Debug.Log("대쉬");
-        
+
     }
     public void DashFalse()
-    { 
-        goDash = false; 
+    {
+        goDash = false;
     }
 
     //p1에서 p2까지의 각도를 계산한다. 이 계산이 왜 이렇게 되는지는 이해가 어렵다 아직
@@ -403,7 +409,7 @@ public class PlayerController : MonoBehaviour
     {
         float angle;
 
-        if(axisH != 0 || axisV != 0)
+        if (axisH != 0 || axisV != 0)
         {
             //p2와 p1의 차를 구하기(원점을 0으로 하기 위해)
             float dx = p2.x - p1.x;
@@ -423,7 +429,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("적과 충돌");
+            //데미지를 받는다
+            GetDamage(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Parry")
         {
             Debug.Log("적과 충돌");
             //데미지를 받는다
@@ -433,12 +445,12 @@ public class PlayerController : MonoBehaviour
     //충돌했을 때 hp가 순식간에 깎이는게 문제
     void GetDamage(GameObject enemy)
     {
-        if(gameState=="playing")
+        if (gameState == "playing")
         {
             Debug.Log("겟 데미지 함수 발동");
             hp--;
             PlayerPrefs.SetInt("PlayerHP", hp); //현재 hp 갱신
-            if(hp>0)
+            if (hp > 0)
             {
                 Debug.Log("체력이 0보다 클 때 겟 데미지 함수 발동");
                 //이동 중지
@@ -454,7 +466,7 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-    
+
     void DamageEnd()
     {
         inDamage = false;
@@ -466,7 +478,7 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("게임오버");
         //게임오버로 만들고
-        //gameState = "gameover";
+        gameState = "gameover";
         GetComponent<CapsuleCollider2D>().enabled = false; //캡슐 콜라이더를 서클 콜라이더로 적용해놓고 안된다고 하고 있었네 ㅋㅋ
         rbody.velocity = new Vector2(0, 0);
         rbody.gravityScale = 1;
@@ -482,5 +494,34 @@ public class PlayerController : MonoBehaviour
         Debug.Log("다운 점프");
 
     }
-     
+
+    public void Parry()
+    {
+        isParry = true;
+        GetComponent<BoxCollider2D>().enabled = true;
+
+        Debug.Log("패링");
+
+    }
+
+    public void ParryEnd()
+    {
+        isParry = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        Debug.Log("패링 끝");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Parry")
+        {
+            Debug.Log("패링 감지");
+            Vector2 jumpPw = new Vector2(0, jump);
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+
+        }
+
+
+    }
+    //클래스 변수에다가 public bool isParry = false; 추가
+    //BoxCollider2D를 추가해서 Exclude Layer에 Everything 체크 후 Parry 체크해서 Parry에만 반응하게
 }
--- Assets/Script/ShootBullet.cs
+++ Assets/Script/ShootBullet.cs
@@ -12,11 +12,11 @@ public class NewBehaviourScript : MonoBehaviour
     bool isAttack = false;
     GameObject spawnBullet;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,12 +32,13 @@ public class NewBehaviourScript : MonoBehaviour
         {
             SpreadAttack();
         }
+
     }
 
     //공격함수
     public void Attack()
     {
-        if(isAttack == false)
+        if (isAttack == false)
         {
             isAttack = true;
             PlayerController playerCnt = GetComponent<PlayerController>();
@@ -52,7 +53,7 @@ public class NewBehaviourScript : MonoBehaviour
             float x = Mathf.Cos(angleZ * Mathf.Deg2Rad);
             float y = Mathf.Sin(angleZ * Mathf.Deg2Rad);
             Vector3 v = new Vector3(x, y) * shootspeed;
-            
+
             /*
             if (Input.GetKey(KeyCode.UpArrow))
             {
@@ -63,55 +64,133 @@ public class NewBehaviourScript : MonoBehaviour
             Rigidbody2D body = peashooter.GetComponent<Rigidbody2D>();
             //총알이 앞으로 가게 근데 이제 여기서 플레이어가 보는 방향에 따라 다르게 가야 ㅎㅎ;
             body.AddForce(v, ForceMode2D.Impulse);
-            
+
             //shootdelay가 좀 빠른 거 같기도 하다
             Invoke("StopAttack", shootdelay);
             Destroy(peashooter, 3.0f);
-            
-            
+
+
         }
-        
+
     }
     public void SpreadAttack()
     {
+
         if (isAttack == false)
         {
             isAttack = true;
             PlayerController playerCnt = GetComponent<PlayerController>();
             float angleZ = playerCnt.angleZ;
+            bool Onground = playerCnt.onGround;
             Quaternion r = Quaternion.Euler(0, 0, angleZ);
-            for(int i = 1; i<5; i++)
+            for (int i = 0; i < 4; i++)
             {
                 GameObject spread = Instantiate(Spread, transform.position, r);
                 float x = Mathf.Cos(angleZ * Mathf.Deg2Rad);
                 float y = Mathf.Sin(angleZ * Mathf.Deg2Rad);
 
-                //밑에 3줄에 뭘 넣어야하나
-                
+                //오른쪽 바라볼 때 발사각
 
-              
-                    Vector3 bulletDir = new Vector3(Mathf.Cos((angleZ-15+15*i) * Mathf.Deg2Rad), Mathf.Sin((angleZ-15+15*i) * Mathf.Deg2Rad)) * shootspeed;
+                if (angleZ == 0)
+                {
+                    Vector3 bulletDir = new Vector3(Mathf.Cos((angleZ + 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
                     Rigidbody2D body = spread.GetComponent<Rigidbody2D>();
-
                     body.AddForce(bulletDir, ForceMode2D.Impulse);
-               
-
-
+                }
                 //bulletDir.x -= 0.3f;
                 //bulletDir.x += 0.3f * i;
                 //Vector3 v = new Vector3(x, y) * shootspeed;
-               
+                //위를 바라볼 때 발사각
+                if (angleZ == 90)
+                {
+                    Vector3 bulletDir90 = new Vector3(Mathf.Cos((angleZ - 30.0f + 20.0f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ - 30.0f + 20.0f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D body90 = spread.GetComponent<Rigidbody2D>();
+                    body90.AddForce(bulletDir90, ForceMode2D.Impulse);
+                }
+                //우대각 일때 발사각
+                if (angleZ == 45)
+                {
+                    Vector3 bulletDir45 = new Vector3(Mathf.Cos((angleZ - 20.0f + 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ - 20.0f + 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D body45 = spread.GetComponent<Rigidbody2D>();
+                    body45.AddForce(bulletDir45, ForceMode2D.Impulse);
+                }
+
+                //왼쪽을 바라볼때 발사각
+                if (angleZ == 180)
+                {
+                    Vector3 bulletDir180 = new Vector3(Mathf.Cos((angleZ - 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ - 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D body180 = spread.GetComponent<Rigidbody2D>();
+                    body180.AddForce(bulletDir180, ForceMode2D.Impulse);
+                }
+
+                //좌대각일 때 발사각
+                if (angleZ == 135)
+                {
+                    Vector3 bulletDir135 = new Vector3(Mathf.Cos((angleZ + 20.0f - 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 20.0f - 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D body135 = spread.GetComponent<Rigidbody2D>();
+                    body135.AddForce(bulletDir135, ForceMode2D.Impulse);
+                }
+
+                //밑으로 발사 N->negative(음수)
+
+                if (angleZ == -90)
+                {
+                    Vector3 bulletDirN90 = new Vector3(Mathf.Cos((angleZ + 30.0f - 20.0f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 30.0f - 20.0f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D bodyN90 = spread.GetComponent<Rigidbody2D>();
+                    bodyN90.AddForce(bulletDirN90, ForceMode2D.Impulse);
+                }
+                //점프가 아니면서 밑을 바라볼때 총알의 회전각도도 바꿔야하는 문제가 생겼다.
+                /*
+                if (Onground && angleZ == -90)
+                {
+                    Vector3 bulletDirON90 = new Vector3(Mathf.Cos((angleZ + 90 + 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 90 + 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D bodyON90 = spread.GetComponent<Rigidbody2D>();
+                    bodyON90.AddForce(bulletDirON90, ForceMode2D.Impulse);
+                }
+                */
+
+                //밑 좌대각
+                
+                if (angleZ == -135)
+                {
+                    Vector3 bulletDirN135 = new Vector3(Mathf.Cos((angleZ - 20.0f + 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ - 20.0f + 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D bodyN135 = spread.GetComponent<Rigidbody2D>();
+                    bodyN135.AddForce(bulletDirN135, ForceMode2D.Impulse);
+                }
+
+                //아래쪽 누른 상태로 왼쪽을 바라볼 때 angleZ 0일 때 발사각도도 angleZ 0이 되는 문제가 생겼다.
+                /*
+                if (Onground && angleZ == -135)
+                {
+                    Vector3 bulletDirON135 = new Vector3(Mathf.Cos((angleZ + 315 - 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 315 - 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D bodyON135 = spread.GetComponent<Rigidbody2D>();
+                    bodyON135.AddForce(bulletDirON135, ForceMode2D.Impulse);
+                }*/
+
+
+
+                //밑 우대각
+                if (angleZ == -45)
+                {
+                    Vector3 bulletDirN45 = new Vector3(Mathf.Cos((angleZ + 20.0f - 11.25f * i) * Mathf.Deg2Rad), Mathf.Sin((angleZ + 20.0f - 11.25f * i) * Mathf.Deg2Rad)) * shootspeed;
+                    Rigidbody2D bodyN45 = spread.GetComponent<Rigidbody2D>();
+                    bodyN45.AddForce(bulletDirN45, ForceMode2D.Impulse);
+                }
+
+
+
+
                 Invoke("StopAttack", shootdelay);
-                Destroy(spread, 1.0f);
+                Destroy(spread, 0.7f);
 
             }
-           
+
 
 
 
 
         }
-            
+
     }
     public void StopAttack()
     {
